da0fed62896475d38e53bb2c1c493e6c9b9ca443
Vinícius Cardoso <viniciuscarimateia@gmail.com>
2026-02-18 10:00:19 -0300

    Ordenacao de Array

---
+++ OrdenacaoArray.java
@@ -0,0 +1,26 @@
+// Desafio 16
+
+public class OrdenacaoArray {
+    public static boolean VerificarArray(int[] array) {
+        if (array == null || array.length <= 1) {
+            return true;
+        }
+
+        boolean crescente = true;
+        boolean decrescente = true;
+
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                crescente = false;
+            }
+            if (array[i] < array[i + 1]) {
+                decrescente = false;
+            }
+        }
+
+        return crescente || decrescente;
+    }
+
+    // Rodar código
+
+}
